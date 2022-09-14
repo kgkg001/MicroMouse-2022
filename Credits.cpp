5cd620761a94b4977fadaea4b310571e9ba648a1
Marcos_ <marcosde@buffalo.edu>
2022-09-14 19:20:53 -0400

    Messing around

---
+++ Credits.cpp
@@ -0,0 +1,10 @@
+//------------------------------------
+// UB IEEE 2022-2023
+// MicroMouse Project Developer Roles
+//------------------------------------
+#include "Credits.h"
+
+credit::credit(int n) : num(n){}
+int credit::displayRoles(){
+    return num;
+}
---
+++ Credits.h
@@ -0,0 +1,15 @@
+//------------------------------------
+// UB IEEE 2022-2023
+// MicroMouse Project Developer Roles 
+//------------------------------------
+#include <stdio.h>
+class credit {
+    private:
+        // roles data structure
+        int num;
+    public:
+        // Constructor
+        credit(int n);
+        // Prints current individuals roles to console
+        int displayRoles();
+};
